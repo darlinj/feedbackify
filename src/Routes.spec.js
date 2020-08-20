e53876988a4c1f126460e6335570eb47b3189686
Joe Darling <joe.darling@bt.com>
2020-08-20 11:57:57 +0100

    Shallow routes tests - crappy ugly tests

--- src/Routes.spec.js
+++ src/Routes.spec.js
@@ -1,46 +1,68 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
-import { MemoryRouter } from "react-router";
+import { Router, MemoryRouter, Route } from "react-router";
+import TestRenderer from "react-test-renderer";
 import Routes from "./Routes";
 import LoginForm from "./LoginForm";
+import ProtectedRoute from "./ProtectedRoute";
 import HomePage from "./HomePage";
 import QuestionsPage from "./QuestionsPage";
 import QuestionnairesPage from "./QuestionnairesPage";
 
 describe("routing", () => {
   it("shows the page to manage questionnaires when the path is / and the user is logged in", () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <MemoryRouter initialEntries={["/"]}>
         <Routes currentUser={{ some: "user" }} />
       </MemoryRouter>
     );
-    expect(wrapper.find(QuestionnairesPage)).toHaveLength(1);
+    expect(
+      wrapper
+        .find(Routes)
+        .dive()
+        .find(HomePage).length
+    ).toEqual(1);
   });
 
   it("shows the welcome page if the use is not logged in", () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <MemoryRouter initialEntries={["/"]}>
         <Routes />
       </MemoryRouter>
     );
-    expect(wrapper.find(HomePage)).toHaveLength(1);
+    expect(
+      wrapper
+        .find(Routes)
+        .dive()
+        .find(HomePage)
+    ).toHaveLength(1);
   });
 
   it("shows the login form when not logged in and trying to access a protected page", () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <MemoryRouter initialEntries={["/questionnaire/123"]}>
         <Routes />
       </MemoryRouter>
     );
-    expect(wrapper.find(LoginForm)).toHaveLength(1);
+    expect(
+      wrapper
+        .find(Routes)
+        .dive()
+        .find(LoginForm)
+    ).toHaveLength(1);
   });
 
   it("shows the page to edit the questionnaire when the path is /questionnaire/123", () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <MemoryRouter initialEntries={["/questionnaire/123"]}>
         <Routes currentUser={{ some: "user" }} />
       </MemoryRouter>
     );
-    expect(wrapper.find(QuestionsPage)).toHaveLength(1);
+    expect(
+      wrapper
+        .find(Routes)
+        .dive()
+        .find(ProtectedRoute)
+    ).toHaveLength(1);
   });
 });
