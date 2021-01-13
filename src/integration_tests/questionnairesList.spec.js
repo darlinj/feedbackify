84a32814edc78bc98e24046368d68d251184fa7f
Joe Darling <joe.darling@bt.com>
2021-01-13 18:34:18 +0000

    trying to seperate tests

--- src/integration_tests/questionnairesList.spec.js
+++ src/integration_tests/questionnairesList.spec.js
@@ -1,30 +1,54 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, cleanup,  screen, fireEvent } from "@testing-library/react";
 
 import App from "../App";
 
-describe("App", () => {
-  beforeAll(async()=> {
-    render(<App />);
-    expect(await screen.findByText(/Please log in/)).toBeInTheDocument();
-    fireEvent.change(screen.getByLabelText("Email"), {
+const loggedInApp = async() => {
+    const app = render(<App />);
+    expect(await app.findByText(/Please log in/)).toBeInTheDocument();
+    fireEvent.change(app.getByLabelText("Email"), {
       target: { value: "pinky@example.com" }
     });
-    fireEvent.change(screen.getByLabelText("Password"), {
+    fireEvent.change(app.getByLabelText("Password"), {
       target: { value: "Passw0rd!" }
     });
-    fireEvent.click(screen.getByText("Login"));
-    expect(await screen.findByText(/log out/)).toBeInTheDocument();
-  })
+    fireEvent.click(app.getByText("Login"));
+    expect(await app.findByText(/log out/)).toBeInTheDocument();
+  return app
+}
+
+describe("App", () => {
+  
+  afterEach(cleanup);
+//  beforeAll(async()=> {
+//    render(<App />);
+//    expect(await screen.findByText(/Please log in/)).toBeInTheDocument();
+//    fireEvent.change(screen.getByLabelText("Email"), {
+//      target: { value: "pinky@example.com" }
+//    });
+//    fireEvent.change(screen.getByLabelText("Password"), {
+//      target: { value: "Passw0rd!" }
+//    });
+//    fireEvent.click(screen.getByText("Login"));
+//    expect(await screen.findByText(/log out/)).toBeInTheDocument();
+//  })
 
   it("logging in", async () => {
-    render(<App />);
-    expect(await screen.findByText(/No questionnaires yet/)).toBeInTheDocument();
+    const app = await loggedInApp();
+    expect(await app.findByText(/No questionnaires yet/)).toBeInTheDocument();
+    fireEvent.click(await app.findByText("Add new questionnaire"))
+    expect(await app.findByText(/Adding new questionnaire/)).toBeInTheDocument();
+    fireEvent.change(app.getByLabelText("Title"), {
+      target: { value: "Some Title" }
+    });
+    fireEvent.click(await app.findByText("Add questionnaire"))
+    expect(await app.findByText(/Some Title/)).toBeInTheDocument();
+    fireEvent.click(await app.findByText("log out"))
   });
 
   it("logging in", async () => {
-    render(<App />);
-    expect(await screen.findByText(/No questionnaires yet/)).toBeInTheDocument();
+    const app = await loggedInApp();
+    expect(await app.findByText(/No questionnaires yet/)).toBeInTheDocument();
     screen.debug()
   });
 });
