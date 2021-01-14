93cf32df784f5eda1cdd798b2724d9871005a574
Joe Darling <joe.darling@bt.com>
2021-01-14 15:24:31 +0000

    fix adding cognito user so it works with renamed environment variables

--- api/tests/createCognitoUser.sh
+++ api/tests/createCognitoUser.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if [[ -z "${TEST_USERNAME}" ]]; then
+if [[ -z "${REACT_APP_TEST_USERNAME}" ]]; then
   USERNAME=${1}
 else
-  USERNAME=${TEST_USERNAME}
+  USERNAME=${REACT_APP_TEST_USERNAME}
 fi
-if [[ -z "${TEST_USER_PASSWORD}" ]]; then
+if [[ -z "${REACT_APP_TEST_USER_PASSWORD}" ]]; then
   PASSWORD=${2}
 else
-  PASSWORD=${TEST_USER_PASSWORD}
+  PASSWORD=${REACT_APP_TEST_USER_PASSWORD}
 fi
-if [[ -z "${TEST_USERNAME}" ]]; then
+if [[ -z "${REACT_APP_TEST_USERNAME}" ]]; then
   PROFILE="--profile admin"
 else
   PROFILE=""
 fi
-if [[ -z "${CLIENT_ID}" ]]; then
+if [[ -z "${REACT_APP_CLIENT_ID}" ]]; then
   echo "FAIL: Cognito CLIENT_ID needs to be set"
 fi
-if [[ -z "${USER_POOL_ID}" ]]; then
+if [[ -z "${REACT_APP_USER_POOL_ID}" ]]; then
   echo "FAIL: Cognito USER_POOL_ID needs to be set"
 fi
 
-aws cognito-idp sign-up --username ${USERNAME} --password ${PASSWORD} --client-id ${CLIENT_ID} --user-attributes Name=phone_number,Value="+441473123456" ${PROFILE}
+aws cognito-idp sign-up --username ${USERNAME} --password ${PASSWORD} --client-id ${REACT_APP_CLIENT_ID} --user-attributes Name=phone_number,Value="+441473123456" ${PROFILE}
 
-aws cognito-idp admin-set-user-password --user-pool-id ${USER_POOL_ID} --username ${USERNAME} --password ${PASSWORD} --permanent ${PROFILE}
+aws cognito-idp admin-set-user-password --user-pool-id ${REACT_APP_USER_POOL_ID} --username ${USERNAME} --password ${PASSWORD} --permanent ${PROFILE}
