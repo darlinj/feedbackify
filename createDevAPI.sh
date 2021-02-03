7a8f4077199e85ee4445dd8f5dab52e4305495f5
Joe Darling <joe.darling@bt.com>
2021-02-03 15:51:52 +0000

    Change URL in the Lambda to point to the URL set in the environment

--- createDevAPI.sh
+++ createDevAPI.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 S3_BUCKET_NAME="pipeline-artifacts-bannana"
 STACK_NAME=${REACT_APP_API_NAME}
+SITE_URL=${REACT_SITE_URL}
 
 if aws s3 ls "s3://$S3_BUCKET_NAME" 2>&1 | grep -q 'NoSuchBucket'
 then
@@ -9,4 +10,4 @@ fi
 
 aws cloudformation package --template-file "api/infra/rootStack.yml" --s3-bucket "${S3_BUCKET_NAME}" --s3-prefix "packagedCloudformationAssetsDEV/" --output-template-file "deploy/packagedTemplate.yml"
 
-aws cloudformation deploy --template-file deploy/packagedTemplate.yml --stack-name ${STACK_NAME}Stack --parameter-overrides APIName=${STACK_NAME} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+aws cloudformation deploy --template-file deploy/packagedTemplate.yml --stack-name ${STACK_NAME}Stack --parameter-overrides APIName=${STACK_NAME} SiteURL=${SITE_URL} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
