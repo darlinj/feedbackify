6d48e25b796a35135b57d5c9475e79dc12ad9ad6
Joe Darling <joe.darling@bt.com>
2021-02-06 11:59:27 +0000

    Initial pipeline with 2 stages

---
+++ pipeline/createPipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+S3_BUCKET_NAME="pipeline-artifacts-bannana"
+PROJECT_NAME="feedbackify"
+
+aws cloudformation package --template-file "./pipeline/pipeline.yml" --s3-bucket "${S3_BUCKET_NAME}" --s3-prefix "packagedPipeline/" --output-template-file "./deploy/packagedPipeline.yml"
+
+aws cloudformation deploy --template-file ./deploy/packagedPipeline.yml --stack-name ${PROJECT_NAME}Stack --parameter-overrides ProjectName=${PROJECT_NAME} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
