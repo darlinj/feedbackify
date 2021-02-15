543be6447386677cf0bca724781a6cc21aa1be64
Joe Darling <joe.darling@bt.com>
2021-02-15 09:07:08 +0000

    Fixing pipeline permissions

--- pipeline/createPipeline.sh
+++ pipeline/createPipeline.sh
@@ -4,4 +4,4 @@ PROJECT_NAME="feedbackify"
 
 aws cloudformation package --template-file "./pipeline/pipeline.yml" --s3-bucket "${S3_BUCKET_NAME}" --s3-prefix "packagedPipeline/" --output-template-file "./deploy/packagedPipeline.yml"
 
-aws cloudformation deploy --template-file ./deploy/packagedPipeline.yml --stack-name ${PROJECT_NAME}Stack --parameter-overrides ProjectName=${PROJECT_NAME} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+aws cloudformation deploy --template-file ./deploy/packagedPipeline.yml --stack-name ${PROJECT_NAME}PipelineStack --parameter-overrides ProjectName=${PROJECT_NAME} --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
