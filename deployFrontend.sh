807d0ab5eaba189ca1175c9cdae20214e8bc6391
Joe Darling <joe.darling@bt.com>
2021-06-30 10:23:09 +0100

    Debugging S3 issues

--- deployFrontend.sh
+++ deployFrontend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 npm run build
+ls -la ./build/*
+ls -la 
 echo "deploying to s3://${ORIGIN_BUCKETNAME}"
 aws s3 sync --cache-control 'max-age=604800' --exclude index.html build/ s3://${ORIGIN_BUCKETNAME} 
 aws s3 sync --cache-control 'no-cache' build/ s3://${ORIGIN_BUCKETNAME} 
